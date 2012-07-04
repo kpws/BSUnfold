167cdaaafdcbf79f4be2dd9740efc6239fe4c4f2
Petter Säterskog <petter.saterskog@gmail.com>
2012-07-04 17:22:06 +0200

    Added multithreading to python program so it now starts many processes. Also changed polyethylen to compound of LOW-MATs.

--- sphere/plot.py
+++ sphere/plot.py
@@ -4,31 +4,47 @@ import numpy as np
 
 def plot(name='default'):
 	E=[]
-	rate6mean=[]
-	rate7mean=[]
-	rate6std=[]
-	rate7std=[]
+	allE=[]
+	allr=[]
+	allRate6Mean=[]
+	allRate7Mean=[]
+	allRate6Std=[]
+	allRate7Std=[]
 	reader=csv.reader(open('results/'+name, 'rb'), delimiter='\t')
 	reader.next()
 	for row in reader:
-		E.append(float(row[0]))
-		n=(len(row)-1)/2
-		rate6=map(float,row[1:n+1])
-		rate7=map(float,row[n+1:])
-		rate6mean.append(np.mean(rate6))
-		rate7mean.append(np.mean(rate7))
-		rate6std.append(np.std(rate6)/np.sqrt(n))
-		rate7std.append(np.std(rate7)/np.sqrt(n))
+		allE.append(float(row[0]))
+		allr.append(float(row[1]))
+		n=(len(row)-2)/2
+		rate6=map(float,row[2:n+1])
+		rate7=map(float,row[n+2:])
+		allRate6Mean.append(np.mean(rate6))
+		allRate7Mean.append(np.mean(rate7))
+		allRate6Std.append(np.std(rate6)/np.sqrt(n))
+		allRate7Std.append(np.std(rate7)/np.sqrt(n))
+	E=[allE[0]]
+	r=[allr[0]]
+	for i in range(len(allE)):
+		if E[-1]!=allE[i]:
+			E.append(allE[i])
+		if r[-1]<allr[i]:
+			r.append(allr[i])
 	
+	foldAndNormalize=lambda l:np.array([[ l[i+len(r)*j]*r[i]**2 for j in range(len(E))] for i in range(len(r))])
+	[rate6Mean,rate7Mean,rate6Std,rate7Std]=map(foldAndNormalize,[allRate6Mean,allRate7Mean,allRate6Std,allRate7Std])
+	neutronRateMean=rate6Mean-rate7Mean
+	neutronRateStd=np.sqrt(rate6Std**2+rate7Std**2)
 	pl.hold(True)
 	nstd=3
-	pl.subplot(111, xscale="log", yscale="log") 
-	pl.errorbar(E, rate6mean, yerr=np.array(rate6std)*nstd, fmt='-',label='Li6F $\pm '+str(nstd)+'\sigma$')
-	pl.errorbar(E, rate7mean, yerr=np.array(rate7std)*nstd, fmt='-',label='Li7F $\pm '+str(nstd)+'\sigma$')
-	pl.legend(loc=4)
+	pl.subplot(111, xscale="log", yscale="linear")
+	for i in range(len(r)):
+		pl.errorbar(E, neutronRateMean[i], yerr=neutronRateStd[i]*nstd, fmt='r-',label='$\pm '+str(nstd)+'\sigma$, r='+str(r[i]))
+		pl.errorbar(E, rate7Mean[i], yerr=rate7Std[i]*nstd, fmt='g-',label='$\pm '+str(nstd)+'\sigma$, r='+str(r[i]))
+	pl.legend(loc=2)
 	pl.xlabel('Beam energy [GeV]')
-	pl.ylabel('Energy / incidence [GeV]')
+	pl.ylabel('Energy / incidence in cm^2[GeV]')
 	pl.show()
 
 if __name__=="__main__":
     plot()
+
--- sphere/simulate.py
+++ sphere/simulate.py
@@ -1,55 +1,90 @@
 import os
 import time
 import numpy as np
+import threading
+from collections import deque
 
 expName='sphere'
 
-def getRate(E,LiMass,r,n=1):
-
+def getRate(E,r,LiMass,n=1,source='',runId=''):
 	replacements={'E':str(E),'LiMass':str(LiMass),'r':str(r),'seed':str(round(time.time()*100))[6:]}
-
-	inFile=open(expName+'.inp','r')
-	os.system('mkdir run')
-	processedFile=open('run/'+expName+'.inp','w')
-	processedFile.write( inFile.read() % replacements )
-	inFile.close()
+	
+	if source=='':
+		inFile=open(expName+'.inp','r');source=inFile.read();inFile.close()
+	os.system('mkdir run'+runId)
+	
+	processedFile=open('run'+runId+'/'+expName+'.inp','w')
+	processedFile.write( source % replacements )
 	processedFile.close()
 	
-	os.system('cd run; rfluka -N0 -M'+str(n)+' '+expName)
+	os.system('cd run'+runId+'; rfluka -N0 -M'+str(n)+' '+expName)
 	result=[]
 	for i in range(n):
 		num=str(i+1)
 		while len(num)<3: num='0'+num
-		resultFile=open('run/'+expName+num+'_fort.21','r')
+		resultFile=open('run'+runId+'/'+expName+num+'_fort.21','r')
 		for j in range(16):
 			resultFile.readline()
 		result.append(float(resultFile.readline()))
 		resultFile.close()
 		
-	os.system('rm -r run')
+	os.system('rm -r run'+runId)
 	return result
-	
-def simulate(name='default',n=1):
+
+class getRateThread(threading.Thread):
+	def __init__(self,E,r,LiMass,n,source,runId):
+		self.E=E
+		self.r=r
+		self.LiMass=LiMass
+		self.n=n
+		self.source=source
+		self.runId=runId
+		threading.Thread.__init__(self)
+	def run(self):
+		self.result=getRate(self.E,self.r,self.LiMass,self.n,self.source,self.runId)
+	def getResult(self):
+		return self.result
+    	
+def simulate(name='default',n=2):
+	inFile=open(expName+'.inp','r');source=inFile.read();inFile.close()
 	os.system('mkdir -p results')
-	EStart=1e-9
-	EEnd=1e3
-	rStart=0.05
-	rEnd=0.3
+	EStart=0.0255e-9
+	EEnd=1e-1
+	rStart=0.03
+	rEnd=0.5
+
+	threads= deque([])
+	E=EStart
+	while E<EEnd:
+		r=rStart
+		while r<rEnd:
+			threads.append(getRateThread(E,r,6,n,source,str(len(threads))))
+			threads.append(getRateThread(E,r,7,n,source,str(len(threads))))
+			r*=1.5
+		E*=1.5
+	
+	runNum=24;
+	
+	for i in range(min(runNum,len(threads))):
+		threads[i].start()
+		
 	resultFile=open('results/'+name,'w')
 	resultFile.write('E\tr\trate6\trate7')
 	resultFile.close()
-	E=EStart
-	r=rStart
-	while E<EEnd:
-		while r<rEnd:
-			resultFile=open('results/'+name,'a')
-			rateCols=''
-			for col in getRate(E,6,r,n)+getRate(E,7,r,n):
-				rateCols+='\t'+str(col)
-			resultFile.write('\n'+str(E)+'\n'+str(r)+rateCols)
-			resultFile.close()
-			r*=1.5
-		E*=2.0
+	while len(threads)!=0:
+		t=threads.popleft()   
+		t.join()
+		if len(threads)>=runNum:
+			threads[runNum-1].start()	
+		
+		resultFile=open('results/'+name,'a')
+		if t.LiMass==6:
+			resultFile.write('\n'+str(t.E)+'\t'+str(t.r))
+		rateCols=''		
+		for col in t.getResult():
+			rateCols+='\t'+str(col)
+		resultFile.write(rateCols)
+		resultFile.close()
 
 if __name__=="__main__":
-    simulate()
+	simulate()

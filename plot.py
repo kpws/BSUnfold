0f09743f3ae0a04fa77692e2b30703465e07b275
Petter Säterskog <petter.saterskog@gmail.com>
2012-08-23 18:57:17 +0200

    added comments

--- plot.py
+++ plot.py
@@ -15,7 +15,7 @@ for j in range(len(milanoReference.names)):
 	pl.plot(milanoReference.E,[mr[j]/s for mr in milanoReference.rate],'o',
 		label=(['CR39, MCNPX by M.Ferrarini']+['']*(len(milanoReference.names)-1))[j])
 lineI+=1
-for detectorName in ['boron10']:#,'tld']:
+for detectorName in ['boron10','tld']:
 	
 	res=result.StatisticResult('results/milano'+detectorName+'Total')
 
@@ -32,14 +32,11 @@ for detectorName in ['boron10']:#,'tld']:
 		i7=detectors.index(7)	
 	else: assert(False)
 	rate=[]
-	std=[]
 	for j in range(1,len(r)):
 		if detectorName=='boron10':
 			rate.append([res.mean[k][j][i]*r[j]**2*np.pi for k in range(len(E))])
-			std.append([res.std[k][j][i]*r[j]**2*np.pi for k in range(len(E))])
 		elif detectorName=='tld':
 			rate.append([(res.mean[k][j][i6]-res.mean[k][j][i7])*r[j]**2*np.pi for k in range(len(E))])
-			std.append([np.sqrt(res.std[k][j][i6]**2+res.std[k][j][i7]**2)*r[j]**2*np.pi for k in range(len(E))])
 		else: assert(False)
 		for k in range(len(rate[-1])):
 			if str(rate[-1][k])=='nan':
@@ -55,6 +52,35 @@ for detectorName in ['boron10']:#,'tld']:
 			pData=res.project([-1,j,i])
 			pl.errorbar(pData[0],np.array(pData[1])*r[j]**2*np.pi/s , yerr=np.array(pData[2])*r[j]**2*np.pi/abs(s), fmt=lines[lineI],
 			label=(['']+[label]+['']*(len(milanoReference.names)))[j],color=colors[j])	
+		elif detectorName=='tld':
+			#########################Fix for different number of Li-6 and Li-7 results, damn ugly###########
+			pData6=[[iii for iii in ii] for ii in res.project([-1,j,i6])]
+			pData7=[[iii for iii in ii] for ii in res.project([-1,j,i7])]
+			ii=0
+			while ii<min(len(pData6[0]),len(pData7[0])):
+				if pData6[0][ii]!=pData7[0][ii]:
+					if pData6[0][ii]<pData7[0][ii]:
+						pData6[0].pop(ii)
+						pData6[1].pop(ii)
+						pData6[2].pop(ii)
+						continue
+					else:
+						pData7[0].pop(ii)
+						pData7[1].pop(ii)
+						pData7[2].pop(ii)
+						continue
+				ii+=1
+			if len(pData6[0])<len(pData7[0]):
+				pData7[0]=pData7[0][:len(pData6[0])]
+				pData7[1]=pData7[1][:len(pData6[1])]
+				pData7[2]=pData7[2][:len(pData6[2])]
+			if len(pData7[0])<len(pData6[0]):
+				pData6[0]=pData6[0][:len(pData7[0])]
+				pData6[1]=pData6[1][:len(pData7[1])]
+				pData6[2]=pData6[2][:len(pData7[2])]
+			####################################End fix#################################################
+			pl.errorbar(pData6[0],(np.array(pData6[1])-np.array(pData7[1]))*r[j]**2*np.pi/s , yerr=np.sqrt(np.array(pData6[2])**2+np.array(pData6[2])**2 )*r[j]**2*np.pi/abs(s), fmt=lines[lineI],
+			label=(['']+[label]+['']*(len(milanoReference.names)))[j],color=colors[j])
 		else: assert(False)
 	lineI+=1
 pl.legend(loc=2)
--- testUnfold.py
+++ testUnfold.py
@@ -7,28 +7,43 @@ import numpy as np
 import milanoReference
 import optimizeFlat
 
+#########################################simulate responses################################
+
+#spectrum, used to calculate responses when no experimental values are available
 spectrum=(CMSNeutrons.ERange, CMSNeutrons.fluence)
-#spectrum=((1e-11,1e0),lambda e:testSpectrum.fluence((1e-11,2e-1),1e-3,1.5,2,0.9,e))
+#spectrum=((1e-11,1e0),lambda e:testSpectrum.fluence((1e-11,2e-1),1e-3,1.5,2,0.9,e))  #bump spectrum
 
-errorEstimate=False
+#what detector is used, can be detector.cr39, detector.tld or detector.tldCr39
+#do not uncomment this when results are availible
 det=detector.cr39
-n=5
-ERange=(5e-13,3e0)
-modelForGuess = Flat(ERange,splitE=optimizeFlat.optimize(det,ERange,n))
-model = Linear(ERange,n)           #This is were the assumption on spectrum shape is made
 
-
+#resp=calculated responses, replace this with actuall responses like this:
+#resp=[no sphere resp, sphere 1 resp, ..., 81=lead response, Linus response]
 resp=det(*spectrum)
-
 print('Expected detector responses:')
 for i in range(len(resp[0])):
 	print((['No sphere']+milanoReference.names)[i]+' & %.1f'%resp[0][i]+' \\\\')
+	
+#########################################Do unfolding################################	
+#number of parameters in model
+n=5
 
+#energy range for model
+ERange=(5e-13,3e0)
 
+#first linear model, used for guess to nonlinear
+#modelForGuess = Flat(ERange,splitE=optimizeFlat.optimize(det,ERange,n)) #use this instead for pptimized base function shapes
+modelForGuess = Flat(ERange,n) 
+#second non-linear model, set to Flat also to get faster results, (or comment everywhere)
+model = Linear(ERange,n)
+
+#set to True for an error estimate, also gives slight error on unfold
+#increase 50 for better accuracy and slower execution, nmbr of MC steps
+errorEstimate=False
 if errorEstimate:
 	guess=[]
 	x=[]
-	for i in range(3):
+	for i in range(4):
 		r=(np.array(resp[0])+[np.random.normal()*ri for ri in resp[1]],resp[1])
 		guess.append([max(0,p) for p in unfold.unfold(det, r, modelForGuess)])
 		x.append(unfold.unfold(det, r, model, guess=np.array(guess[-1])))
@@ -42,16 +57,20 @@ else:
 	x=unfold.unfold(det, resp, model, guess=guess*2)
 	guessErrors=[0]*len(guess)
 	errors=[0]*len(x)
-print('Base weights:')
-#for xp in x:
-#	print(str(xp))
+
+print('Optimal parameters:')
+for xp in x:
+	print(str(xp))
 	
-resp2=det(modelForGuess.getERange(), model.getFluence(x))#TODO guess-> x
+#print responses in latex table friendly way
+respx=det(model.getERange(), model.getFluence(x))
+respguess=det(modelForGuess.getERange(), modelForGuess.getFluence(x))
 print('Expected detector responses with calculated spectrum:')
-for i in range(len(resp2[0])):
-	print((['No sphere']+milanoReference.names)[i]+' & %.1f'%resp[0][i]+' & %.1f'%resp2[0][i]+' \\\\')
+for i in range(len(respx[0])):
+	print((['No sphere']+milanoReference.names)[i]+' & %.1f'%resp[0][i]+' & %.1f'%respguess[0][i]+' & %.1f'%respx[0][i]+' \\\\')
 	
-	
+
+#plot stuff!!!
 import pylab as pl
 pl.subplot(111, xscale="log", yscale="log")
 pn=1e3

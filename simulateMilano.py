76797108197c16a2b3003cf422230f0509b4a601
Petter Säterskog <petter.saterskog@gmail.com>
2012-08-23 15:33:29 +0200

    cleaned up some mess

--- simulateMilano.py
+++ simulateMilano.py
@@ -2,24 +2,25 @@ import simulate
 import milanoReference
 import numpy as np
 
-#names=['2.0']+milanoReference.names
+#use last two spheres
 names=milanoReference.names[-2:]
+
+#name '2.0' means no sphere, the name is used because the beam has this size.
+#Uncomment next line to use.
+#names=['2.0']+milanoReference.names
+
 E=[]
 for i in range(len(milanoReference.E)):
 	E.append(milanoReference.E[i])
 	if i!=len(milanoReference.E)-1:
 		E.append(np.sqrt(milanoReference.E[i]*milanoReference.E[i+1]))
 
-E+=[1.0e-16,1.0e-14,1.0e-13,3.0e-13,1.0e-12,3.0e-12,1.0e-11,3e0,1e1]
+E+=[1.0e-16,1.0e-14,1.0e-13,3.0e-13,1.0e-12,3.0e-12,1.0e-11,3e0]
 rho=0.96
 runs=6
 partNum=2e6
-detectors=['boron10','tld']
+detectors=['tld'] #['boron10','tld']
 runNum=10 #We have 12 cpus
 
 for d in detectors:
 	simulate.simulate('milano'+d,runNum,runs,partNum, names[-2:],E,rho,d)
-
-#['4.0','81+lead','Linus']
-#for d in detectors:
-#	simulate.simulate('test'+d,3,1,1e4,['Linus'] ,[5e-1],rho,d)
